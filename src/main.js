c8bcbff82ee6c20df651abc3b81ca42922b54ea9
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-01-15 23:42:41 +0800

    Update table

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import DataTable from 'datatables.net-dt'
+import moment from 'moment'
 import './styles/main.scss'
 
 const response = await fetch('https://json.extendsclass.com/bin/046f024ee1d1')
@@ -5,4 +7,38 @@ const response = await fetch('https://json.extendsclass.com/bin/046f024ee1d1')
   .then((data) => data)
   .catch((error) => console.log(error))
 
-console.log(response.data)
+const table = document.querySelector('table')
+
+const dataTable = new DataTable(table, {
+  columns: [
+    { data: 'sellerName' },
+    {
+      data: 'ratingPercentage',
+      render(data) {
+        return `${data}%`
+      },
+    },
+    { data: 'ratingCount' },
+    {
+      data: 'lastWonUTC',
+      render(data) {
+        return moment.utc(data).format('M/D/YY HH:mm')
+      },
+    },
+    {
+      data: 'winDurationSeconds',
+      render(data) {
+        return moment.duration(data, 'seconds').humanize()
+      },
+    },
+  ],
+  data: response.data,
+  info: false,
+  lengthChange: false,
+  pageLength: 5,
+  searching: false,
+})
+
+if (table) {
+  dataTable.init()
+}
