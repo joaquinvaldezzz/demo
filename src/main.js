18b9bb01b9b2fc6b3100da8641b9a0b35b63eb21
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-01-15 23:51:20 +0800

    Add comments

--- src/main.js
+++ src/main.js
@@ -2,19 +2,24 @@ import DataTable from 'datatables.net-dt'
 import moment from 'moment'
 import './styles/main.scss'
 
+// Fetch the data from the API
 const response = await fetch('https://json.extendsclass.com/bin/046f024ee1d1')
   .then((reply) => reply.json())
   .then((data) => data)
   .catch((error) => console.log(error))
 
+// Get the `table` element
 const table = document.querySelector('table')
 
+// Initialize DataTable on `table` element
 const dataTable = new DataTable(table, {
+  // Define the columns to display
   columns: [
     { data: 'sellerName' },
     {
       data: 'ratingPercentage',
       render(data) {
+        // Add a percentage symbol using a template literal
         return `${data}%`
       },
     },
@@ -22,23 +27,28 @@ const dataTable = new DataTable(table, {
     {
       data: 'lastWonUTC',
       render(data) {
+        // Format the date using moment.js
         return moment.utc(data).format('M/D/YY HH:mm')
       },
     },
     {
       data: 'winDurationSeconds',
       render(data) {
+        // Format the duration using moment.js
         return moment.duration(data, 'seconds').humanize()
       },
     },
   ],
-  data: response.data,
+  data: response.data, // Use the data from the API response
+  pageLength: 5, // Show 5 rows per page
+
+  // Disabled some features that are not needed
   info: false,
   lengthChange: false,
-  pageLength: 5,
   searching: false,
 })
 
+// If the `table` exists, initialize DataTable
 if (table) {
   dataTable.init()
 }
